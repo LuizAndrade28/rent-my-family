91a21b3eaeef431df286de8d548400acdf8de6a0
LuizAndrade <lcfsandrade@gmail.com>
2024-02-04 21:04:09 -0300

    Parte 2 - revisão geral e ajuste do back-end

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,16 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :family_member
   has_one :review, dependent: :destroy
+
+  validates :quantity_hour, :rent_date, presence: true
+  validates :quantity_hour, numericality: { greater_than: 0 }
+  validate :rent_date_cannot_be_in_the_past
+
+  private
+
+  def rent_date_cannot_be_in_the_past
+    if rent_date.present? && rent_date < Date.today
+      errors.add(:rent_date, "can't be in the past")
+    end
+  end
 end
